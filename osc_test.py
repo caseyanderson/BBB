9f9b0604de949b84555984d9c022f557ac03c275
caseyanderson <casey.thomas.anderson@gmail.com>
2014-07-13 14:35:07 -0700

    added osc_test.py example

---
+++ osc_test.py
@@ -0,0 +1,20 @@
+#SIMPLE OSC TEST
+#sourced from here: https://github.com/redFrik/udk10-Embedded_Systems/tree/master/udk131128
+
+#this sends an osc message to sc every second
+#start it with 'python osctest.py &'
+
+import time
+import OSC
+
+sc= OSC.OSCClient()
+sc.connect(('127.0.0.1', 57120))
+while True:
+        msg= OSC.OSCMessage()
+        msg.setAddress("/ping")
+        msg.append(111)
+        try:
+                sc.send(msg)
+        except:
+                1+1 # dummy
+        time.sleep(1)
